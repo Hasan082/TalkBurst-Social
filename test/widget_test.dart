e6ae278b77b89aff3e802b6e519f51117b4d95a0
Md Hasanuzzaman <dr.has82@gmail.com>
2023-12-01 05:55:10 +0600

    Tester added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:talkbrust/application/app.dart';
@@ -22,7 +15,7 @@ void main() {
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pump();  // Trigger a frame to rebuild the widget tree.
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
