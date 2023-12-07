9ea51703564a67e335668709cecf45a1b0cf89a2
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-12-07 23:44:22 +0600

    Comment Bottom sheet added

--- lib/widgets/custom_appbar/home_card.dart
+++ lib/widgets/custom_appbar/home_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'custom_avater.dart';
 class HomeCard extends StatelessWidget {
   const HomeCard({
@@ -39,13 +38,23 @@ class HomeCard extends StatelessWidget {
             ),
             const SizedBox(height: 10,),
             Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(width: 10,),
                 Image.asset("assets/images/heart.png"),
                 const SizedBox(width: 8,),
-                Image.asset("assets/images/message.png"),
-                const SizedBox(width: 8,),
-                const Text("20 comments"),
+                InkWell(
+                  onTap: (){
+                    _modalBottomSheetMenu(context);
+                  },
+                  child:Row(
+                    children: [
+                      Image.asset("assets/images/message.png"),
+                      const SizedBox(width: 8,),
+                      const Text("20 comments"),
+                    ],
+                  ) ,
+                ),
                 const SizedBox(width: 8,),
                 const Spacer(),
                 Image.asset("assets/images/bookmark.png"),
@@ -77,4 +86,100 @@ class HomeCard extends StatelessWidget {
       ),
     );
   }
+
+
+  void _modalBottomSheetMenu(context){
+    var size=MediaQuery.sizeOf(context);
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10)
+        ),
+
+        builder: (builder){
+          return Padding(
+            padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: SizedBox(
+              height: size.height/2,
+              width: double.infinity,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 0),
+                    child: Text("Comment",style:Theme.of(context).textTheme.titleSmall,),
+                  ),
+                  const Divider(),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: 4,
+                        itemBuilder: (context,index){
+                          return  ListTile(
+                            leading: const CircleAvatar(
+                                radius: 20,
+                                backgroundImage: NetworkImage("https://maruf136.000webhostapp.com/image/maruf.jpeg")
+                            ),
+                            title: Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: Text("mferdos12",style: Theme.of(context).textTheme.titleSmall,),
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                const SizedBox(height: 10,),
+                                Text("Nice Picture You have capture",style:Theme.of(context).textTheme.bodySmall,),
+                                const SizedBox(height: 8,),
+                                InkWell(
+                                    onTap: (){},
+                                    child: const Text("Replay")
+                                )
+                              ],
+                            ),
+                            trailing: Image.asset("assets/images/heart_outline.png",width: 35,),
+                          );
+                        }
+                    ),
+                  ),
+                  const Divider(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 20),
+                    child: Row(
+                      children: [
+                        const CustomCircularAvater(),
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: "Add comments",
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                              disabledBorder: InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(horizontal: 15,vertical: 5),
+                            ),
+                            autofocus: true,
+                          ),
+                        ),
+                        TextButton(
+                            onPressed: (){},
+                            child: Text("Post",style: Theme.of(context).textTheme.titleSmall?.apply(color: Colors.grey),)
+                        )
+                      ],
+                    ),
+                  )
+
+
+                ],
+              ),
+            ),
+          );
+        }
+    );
+  }
+
+
+
+
 }
